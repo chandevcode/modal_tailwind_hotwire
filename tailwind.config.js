81dd9b49f65c3aefa256f3be8c0dc7268d93f7ac
Putu Chandra <baguschandradp@gmail.com>
2023-04-05 12:36:35 +0800

    add modal hotwire

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,28 @@
 module.exports = {
+  daisyui: {
+    themes: false,
+  },
+  theme: {
+    extend: {
+      keyframes: {
+        'fade-in-up': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(10px)'
+          },
+          '100%': {
+            opacity: '0',
+            transform: 'translateY(0)'
+          },
+        }
+      },
+      animation: {
+        'fade-in-up': 'fade-in-up 0.5 ease-in-out'
+
+      }
+    }
+
+  },
   plugins: [
     require('daisyui')
   ],
